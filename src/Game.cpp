38f59e588d2222a0eef96068ae465aa31877958e
jwhidte <123980160+jwhidte@users.noreply.github.com>
2023-03-07 18:13:52 -0500

    code core

--- src/Game.cpp
+++ src/Game.cpp
@@ -8,6 +8,7 @@ int SIZEX = 64, SIZEY = 64;
 
 //SDL Variables
 SDL_Renderer* Game::renderer = nullptr;
+SDL_Window*& Game::window = nullptr;
 SDL_Event Game::event;
 Sprite player = Sprite(100, 100, 64, 64);
 
@@ -16,7 +17,7 @@ vector<vector<int>> Game::tilemap;
 bool Game::running = false;
 Media Game::media;
 // Tilemap variables
-int mapWidth, mapHeight;
+int mapWidth, mapHeight = 20;
 
 Game::Game()
 {}
@@ -42,17 +43,26 @@ void Game::init(const char* title, int width, int height, bool fullscreen)
     }
     if (SDL_Init(SDL_INIT_EVERYTHING) == 0)
     {
-        window = SDL_CreateWindow(title, SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, width, height, SDL_WINDOW_RESIZABLE);
-        renderer = SDL_CreateRenderer(window, -1, 0);
-        if (renderer)
+        Game::window = SDL_CreateWindow(title, SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, width, height, SDL_WINDOW_RESIZABLE);
+        Game::renderer = SDL_CreateRenderer(Game::window, -1, 0);
+        if(!Game::window)
         {
-            SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+            printf("epic window fail");
+        }
+        if (Game::renderer)
+        {
+            SDL_SetRenderDrawColor(Game::renderer, 0, 0, 0, 255);
+        }
+        else
+        {
+            printf("epic renderer fail");
         }
         running = true;
     }
     else
     {
         running = false;
+        printf("epic fail");
     }
 	if (Mix_OpenAudio( 44100, MIX_DEFAULT_FORMAT, 2, 1024 ) < 0 ) 
     {
@@ -66,7 +76,7 @@ void Game::init(const char* title, int width, int height, bool fullscreen)
     Game::media.addTexture("tile","images/tile.png");
     Game::media.addTexture("face1","images/Face1.png");
     Game::media.addTexture("face2","images/Face2.png");
-    Game::tilemap = media.loadTilemap("images/map.txt");
+    Game::tilemap = media.loadTilemap("images/tilemap.txt");
 }
 void Game::handleEvents()
 {
@@ -94,15 +104,27 @@ void Game::update()
 }
 void Game::render()
 {
-    SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
-    SDL_RenderClear(renderer);
+    SDL_SetRenderDrawColor(Game::renderer, 0, 0, 0, 255);
+    SDL_RenderClear(Game::renderer);
     player.render();
+    for (int y = 0; y < mapHeight; y++)
+    {
+        for (int x = 0; x < mapWidth; x++)
+        {
+            if (tilemap[y][x] == 1)
+            {
+                SDL_Rect tileRect = { x * TILE_SIZE, y * TILE_SIZE, TILE_SIZE, TILE_SIZE };
+                SDL_RenderCopy(Game::renderer, Game::media.getTexture("tile"), nullptr, &tileRect);
+            }
+        }
+    }
     SDL_RenderPresent(Game::renderer);
     SDL_Delay(1000/FPS);
+
 }
 void Game::clean()
 {
-    SDL_DestroyWindow(window);
-    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(Game::window);
+    SDL_DestroyRenderer(Game::renderer);
     SDL_Quit();
 }
